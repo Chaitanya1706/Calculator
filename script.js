8304b7a29d37b4f806e67180333b4b26fd513d75
Chaitanya <17agrawalchaitanya@gmail.com>
2021-12-04 12:38:04 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,63 @@
+var btns = document.getElementsByClassName('btn');
+var display = document.getElementById('display-value')
+var operator_value = document.getElementById('operator-value')
+var operand1 = 0;
+var operand2 = null;
+var operator = null;
+var flag = false;
+
+function run(){
+    var value = this.getAttribute('data-value');
+    
+    if(value=='AC'){
+        display.innerText = '';
+        operator_value.innerText = '';
+        operand1 = 0;
+        operand2 = null;
+        operator = null;
+    }
+
+    if(value=='+' || value=='-' || value=='*' || value=='/' || value=='%'){
+        if(operator!=null){
+            operator = value;
+            operator_value.innerText = operator;
+        }
+        else{
+            operator = value;
+            flag = true;
+            if(display.innerText=='' || display.innerText=='AC'){
+                display.innerText = 'Error';
+            }
+            operator_value.innerText = operator;
+            if(operand1!=null){
+                var ans = eval(operand1 + " " + operator + " " + parseFloat(display.innerText));
+                operand1 = ans;
+            }
+            else
+                operand1 = parseFloat(display.innerText);
+        }
+    }
+    else if(value=='='){
+        operator_value.innerText = '';
+        operand2 = parseFloat(display.innerText);
+        var ans = eval(operand1 + " " + operator + " " + operand2);
+        display.innerText = ans;
+    }
+    else{
+    
+        if(display.innerText=='Error' || display.innerText=='AC'){
+            display.innerText = '';
+        }
+
+        if(flag==true){
+            display.innerText = '';
+            flag = false;
+        }
+
+        display.innerText += value;
+    }
+}
+
+for(var i = 0;i<btns.length;i++){
+    btns[i].addEventListener('click',run);
+}
