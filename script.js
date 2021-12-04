26204ec0a9927e1d9db35155747a1da838eba39a
Chaitanya <17agrawalchaitanya@gmail.com>
2021-12-04 16:42:10 +0530

    Handled some error

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var operand2 = null;
 var operator = null;
 var flag = false;
 var op = null;
+var afterAns = false;
 
 function run(){
     var value = this.getAttribute('data-value');
@@ -17,27 +18,53 @@ function run(){
         operand2 = null;
         operator = null;
         op=null;
+        afterAns = false;
+        flag = false;
+    }
+
+    else if(value=='delete'){
+        if(afterAns==true){
+            operand1 = null;
+            operand2 = null;
+            operator = null;
+            op=null;
+            afterAns = false;
+            flag = false;
+        }
+        var str = display.innerText;
+        var l = str.length;
+        display.innerText = str.substring(0,l-1);
+        
     }
 
     else if(value=='-' || value=='+' || value=='*' || value=='/' || value=='%'){
-        if(op!=null){
-            operator = value;
-            operator_value.innerText = operator;
+        if(display.innerText==''&&value=='-'){
+            display.innerText += value;
+            
         }
         else{
-            operator = value;
-            op = value;
-            flag = true;
-            if(display.innerText=='' || display.innerText=='AC'){
-                display.innerText = 'Error';
+            if(op!=null){
+                operator = value;
+                operator_value.innerText = operator;
             }
-            operator_value.innerText = operator;
-            if(operand1!=null){
-                var ans = eval(operand1 + " " + operator + " " + parseFloat(display.innerText));
-                operand1 = ans;
+            else{
+                
+                op = value;
+                if(display.innerText=='' || display.innerText=='AC'){
+                    display.innerText = 'Error';
+                }
+                operator_value.innerText = op;
+                if(operand1!=null){
+                    var ans = eval(operand1 + " " + operator + " " + parseFloat(display.innerText));
+                    operand1 = ans;
+                    display.innerText = ans;
+                }
+                else
+                    operand1 = parseFloat(display.innerText);
+                
+                operator = value;
+                flag = true;
             }
-            else
-                operand1 = parseFloat(display.innerText);
         }
     }
     else if(value=='='){
@@ -45,8 +72,18 @@ function run(){
         operand2 = parseFloat(display.innerText);
         var ans = eval(operand1 + " " + operator + " " + operand2);
         display.innerText = ans;
+        afterAns = true;
     }
     else{
+        if(afterAns==true){
+            display.innerText = '';
+            operator_value.innerText = '';
+            operand1 = null;
+            operand2 = null;
+            operator = null;
+            op=null;
+        }
+        afterAns = false;
         op = null;
         if(display.innerText=='Error' || display.innerText=='AC'){
             display.innerText = '';
